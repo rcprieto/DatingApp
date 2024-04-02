9b9a58ae83520646d14390e71028a1d7cdc757ae
gosoftwares <rcprieto@gmail.com>
2024-04-02 10:06:23 -0300

    first commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+	[Key]
+	public int Id { get; set; }
+
+	[StringLength(300)]
+	public string UserName { get; set; }
+
+
+}
